479ea91074aef703d812e3ecfec054de9d224130
amixhh <raoamish004@gmail.com>
2025-01-07 20:11:13 +0530

    fixed mobile responsiveness issues

--- script.js
+++ script.js
@@ -1,38 +1,72 @@
-const inputBox = document.getElementById("task")
+const inputBox = document.getElementById("task");
 
-function addTask(){
-    if(inputBox.value === ""){
-        alert("You must write something Nigga !!")
-    }
-    else{
+function addTask() {
+    if (inputBox.value === "") {
+        alert("You must write something nigger!!");
+    } else {
         let li = document.createElement("li");
-        li.innerHTML =   inputBox.value;
+        li.innerHTML = inputBox.value;
         document.getElementById("list").appendChild(li);
+
         let span = document.createElement("span");
-        span.innerHTML = "\u00d7"
+        span.innerHTML = "\u00d7"; 
         li.appendChild(span);
-
     }
-    inputBox.value = ""
-    saveData()
+    inputBox.value = "";
+    saveData();
 }
 
-document.getElementById("list").addEventListener("click" , (e)=>{
-    if(e.target.tagName === "LI"){
+document.getElementById("list").addEventListener("click", (e) => {
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
-        saveData()
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
     }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove()
-        saveData()
-    }
-})
+});
 
-function saveData(){
-    localStorage.setItem("data" , list.innerHTML);
+// Edit task on double-click
+document.getElementById("list").addEventListener("dblclick", (e) => {
+    if (e.target.tagName === "LI") {
+        let currentText = e.target.childNodes[0].nodeValue.trim(); 
+        let input = document.createElement("input");
+        input.type = "text";
+        input.value = currentText;
+        e.target.innerHTML = ""; 
+        e.target.appendChild(input);
+
+        input.addEventListener("keypress", (event) => {
+            if (event.key === "Enter") {
+                saveEditedTask(e.target, input.value);
+            }
+        });
+
+        input.addEventListener("blur", () => {
+            saveEditedTask(e.target, input.value);
+        });
+
+        input.focus();
+    }
+});
+
+function saveEditedTask(li, newValue) {
+    li.innerHTML = newValue;
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7"; 
+    li.appendChild(span);
+    saveData();
 }
 
-function showData(){
-    list.innerHTML = localStorage.getItem("data")
+function saveData() {
+    localStorage.setItem("data", document.getElementById("list").innerHTML);
 }
-showData()
+
+function showData() {
+    const savedData = localStorage.getItem("data");
+    if (savedData) {
+        document.getElementById("list").innerHTML = savedData;
+    }
+}
+
+showData();
